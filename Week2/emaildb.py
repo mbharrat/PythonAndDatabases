341236f21d7bb7ee27223a2ef8acc61731ef7297
mbharrat <mbharrat@gmail.com>
2017-01-09 16:53:46 -0500

    working on week 2 assignments

---
+++ Week2/emaildb.py
@@ -0,0 +1,45 @@
+import sqlite3
+#creates sql databse file called emaildb.sqlite
+conn = sqlite3.connect('emaildb.sqlite')
+cur = conn.cursor()
+
+cur.execute('''
+DROP TABLE IF EXISTS Counts''')
+
+cur.execute('''
+CREATE TABLE Counts (email TEXT, count INTEGER)''')
+
+fname = raw_input('Enter file name: ')
+if ( len(fname) < 1 ) : fname = 'mbox-short.txt'
+fh = open(fname)
+for line in fh:
+    if not line.startswith('From: ') : continue
+    pieces = line.split()
+    email = pieces[1]
+    print email
+    cur.execute('SELECT count FROM Counts WHERE email = ? ', (email, ))
+    #? in email = is placeholder to be filled in w/ whatever you need
+    #first thing in tuple is whats in question mark
+    #this is done to prevent sql injection
+    row = cur.fetchone()
+    if row is None:
+        cur.execute('''INSERT INTO Counts (email, count) 
+                VALUES ( ?, 1 )''', ( email, ) )
+    else : 
+        cur.execute('UPDATE Counts SET count=count+1 WHERE email = ?', 
+            (email, ))
+    # This statement commits outstanding changes to disk each 
+    # time through the loop - the program can be made faster 
+    # by moving the commit so it runs only after the loop completes
+    conn.commit()
+
+# https://www.sqlite.org/lang_select.html
+#basically prints to screen when table is created and set up
+sqlstr = 'SELECT email, count FROM Counts ORDER BY count DESC LIMIT 10'
+
+print
+print "Counts:"
+for row in cur.execute(sqlstr) :
+    print str(row[0]), row[1]
+
+cur.close()
