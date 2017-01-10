5fe4f16e65fcff540afa77f68b69c2756f034c73
mbharrat <mbharrat@gmail.com>
2017-01-10 12:55:07 -0500

    started work on org name db python file

--- Week2/myemaildb.py
+++ Week2/myemaildb.py
@@ -1,5 +1,13 @@
 #import statment to use sql
 import sqlite3
 
+#create sql database file called myemaildb.sql
+conn = sqlite3.connect('myemaildb.sql')
+curr = conn.cursor()
 
+cur.execute('''
+DROP TABLE IF EXISTS Counts''')
+
+cur.execute('''
+CREATE TABLE Counts (org TEXT, count INTEGER)''')
 
